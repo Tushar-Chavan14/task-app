151daf76fd660112159ae6961ce3576a85800f11
Tushar-Chavan14 <tusharchavan166@gmail.com>
2023-04-13 00:47:06 +0530

    mail

--- src/routers/userRouters.js
+++ src/routers/userRouters.js
@@ -12,7 +12,7 @@ userRouter.post("/users", async (req, res) => {
 
   try {
     await user.save();
-    welcomeMail(user.email);
+    await welcomeMail(user.email);
     const token = await user.genrateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
